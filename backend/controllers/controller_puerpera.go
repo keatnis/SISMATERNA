1909f3b88cfd0472ee50071cc0c51a34d1995619
rociog87 <galvezr51@gmail.com>
2023-02-23 19:01:02 -0600

    kk

--- backend/controllers/controller_puerpera.go
+++ backend/controllers/controller_puerpera.go
@@ -11,13 +11,13 @@ func InsertPuerpera(c ty.Puerpera) error {
 		return err
 	}
 
-	sentenciaPreparada, err := db.Prepare("INSERT INTO puerpera (consulta_siete, consulta_veintiocho, consulta_cuarenta) VALUES(?,?,?)")
+	sentenciaPreparada, err := db.Prepare("INSERT INTO puerpera (consulta_siete, consulta_veintiocho, consulta_cuarenta, signos_alarma, atencion_parto, lugar_parto, resolucion, no_producto, apeo, puerpera_aceptante, consulta_pregestacional) VALUES(?,?,?,?,?,?,?,?,?,?,?)")
 	if err != nil {
 		return err
 	}
 	defer sentenciaPreparada.Close()
 	// Ejecutar sentencia, un valor por cada '?'
-	_, err = sentenciaPreparada.Exec(c.Consultasiete, c.Consultaveinte, c.Consultacuarenta)
+	_, err = sentenciaPreparada.Exec(c.Consultasiete, c.Consultaveinte, c.Consultacuarenta, c.Signos, c.Atencionparto, c.Lugarparto, c.Resolucion, c.Producto, c.Apeo, c.Aceptante, c.Pregestacional)
 	if err != nil {
 		return err
 	}
